ccb81a4e86106f623c9fe7a49fc9abe7cb9ab033
advityaggarwal <42826882+advityaggarwal@users.noreply.github.com>
2018-08-31 00:58:11 +0530

    Second Commit

--- information.component.ts
+++ information.component.ts
@@ -6,13 +6,20 @@ import {InformationService} from '../information.service'
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
-  styleUrls: ['./information.component.css']
+  
 })
 export class InformationComponent implements OnInit {
 
   constructor(private _informationService:InformationService) { }
   information:IData[];
+  clicked:boolean=false;
 
+  empId:number;
+  empName:string;
+  empSal:number;
+  empDep:string;
+  empjoiningdate:string;
+  index2:number;
 
   ngOnInit() {
     this._informationService.getInfo().subscribe(data=>this.information=data);
@@ -39,15 +46,36 @@ export class InformationComponent implements OnInit {
   }
   sortByjoiningdate()
   {
-    this.information.sort((a,b)=>/*{a = new Date(a.empjoiningdate);
-    b = new Date(b.empjoiningdate);
-    return a>b ? -1 : a<b ? 1 : 0;}*/{var c=new Date(a.empjoiningdate);var d=new Date(b.empjoiningdate);
-      return c-d;});
+    this.information.sort(function(a,b){
+      var c:any=a.empjoiningdate.toString().split('/');
+      var d:any=b.empjoiningdate.toString().split('/');
+      return c[2]-d[2] || c[1]-d[1] || c[0]-d[0]
+    });
   }
 
   delete(x:number)
   {
     this.information.splice(x,1);
   }
+  update(y:number)
+  {
   
+    this.empId=this.information[y].empId;
+    this.empName=this.information[y].empName;
+    this.empSal=this.information[y].empSal;
+    this.empDep=this.information[y].empDep;
+    this.empjoiningdate=this.information[y].empjoiningdate;
+    this.index2=y;
+    this.clicked=!this.clicked;
+  }
+  validate()
+  {
+    let j=this.information.findIndex((a)=>a.empId==this.empId);
+    this.information[j].empId=this.empId;
+    this.information[j].empName=this.empName;
+    this.information[j].empSal=this.empSal;
+    this.information[j].empDep=this.empDep;
+    this.information[j].empjoiningdate=this.empjoiningdate;
+    this.clicked=!this.clicked;
+  }
 }
